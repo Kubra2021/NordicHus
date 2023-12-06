047dd7e12ff9ba483312e7f4569db2d74503b0fe
KubraErcin <121133657+Kubra2021@users.noreply.github.com>
2023-12-06 22:34:19 +0100

    First

---
+++ script.js
@@ -0,0 +1,13 @@
+function goToPage(page) {
+    // Sayfa geçişleri burada işlenir
+    console.log("Go to " + page);
+    // Burada sayfa içeriğini güncelleyebilirsiniz
+}
+
+function goToLoginPage() {
+    // Giriş sayfasına geçiş işlemleri burada işlenir
+    console.log("Go to Login page");
+    // Burada sayfa içeriğini güncelleyebilirsiniz
+}
+
+
