a8656a003cdb34c23d21e20690ae3ebe7702ab92
Esra Unal <118307139+esraunal07@users.noreply.github.com>
2023-12-07 23:49:16 +0100

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function goToPage(page) {
-    // Sayfa geçişleri burada işlenir
-    console.log("Go to " + page);
-    // Burada sayfa içeriğini güncelleyebilirsiniz
+  console.log("Go to " + page);
 }
 
 function goToLoginPage() {
-    // Giriş sayfasına geçiş işlemleri burada işlenir
     console.log("Go to Login page");
-    // Burada sayfa içeriğini güncelleyebilirsiniz
 }
+
+function scrollDown() {
+    const mainContent = document.querySelector("index");
+    mainContent.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  
 
 
