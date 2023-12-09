f191885e8e0872f4c5835fb82d48d7e8824ff89d
necla <123477628+necla87@users.noreply.github.com>
2023-12-09 22:13:20 +0100

    improved the codes for listing the properties

--- script1.js
+++ script1.js
@@ -10,20 +10,10 @@ fetch('bostadlist.json')
 
     // Search function
     function searchProperties() {
-      var propertyType = document.getElementById('propertyType').value;
-      var minPrice = document.getElementById('minPrice').value;
-      var maxPrice = document.getElementById('maxPrice').value;
-
-      var filteredProperties = properties.filter(function (property) {
-        return (!propertyType || property.type === propertyType) &&
-          (!minPrice || property.price >= minPrice) &&
-          (!maxPrice || property.price <= maxPrice);
-      });
-
-      displayProperties(filteredProperties);
+      // ... (unchanged code) ...
     }
 
-    // Display function
+    // Function to display properties
     function displayProperties(properties) {
       console.log(properties); // Log the properties to the console
       var propertyList = document.getElementById('propertyList');
@@ -34,21 +24,35 @@ fetch('bostadlist.json')
       } else {
         properties.forEach(function (property) {
           var propertyItem = document.createElement('li');
+          var additionalLabels = '';
+
+          if (property.elevator) {
+            additionalLabels += '<div class="additional-label">Elevator</div>';
+          }
+
+          if (property.balcony) {
+            additionalLabels += '<div class="additional-label-container"><div class="additional-label">Balcony</div></div>';
+          }
+
           propertyItem.innerHTML = `
-          <div class="property-box">
+        <div class="property-box">
           <img src="${property.image}" alt="Property Image" class="property-image">
-          <div class="property-info">
-          <p>ID: ${property.id}</p>
-          <p>Type: ${property.type}</p>
-          <p>Address: ${property.address}</p>
-          <p>Price: $${property.price}</p>
-          <p>Rooms: ${property.rooms}</p>
+          <div class="property-info-description">
+            <p>${property.type}</p>
+            <p>${property.address}</p>
+            <p>${property.price} SEK | ${property.area} sq. ft. | ${property.rooms} Rooms</p>
+            ${additionalLabels}
+            <hr>
+            <p>${property.type === 'House' ? 'House' : 'Apartment'} located in ${property.address} is now available for sale.</p>
+            <p>This ${property.rooms}-bedroom property has a total area of ${property.area} square feet, offering spacious living areas. It features ${property.balcony ? 'a balcony, ' : ''}${property.elevator ? 'an elevator, ' : ''}${property.storage ? 'storage, ' : ''}${property.parking ? 'parking, ' : ''}${property.courtyard ? 'a courtyard, ' : ''}making it a convenient and comfortable home. Don't miss the opportunity to own this property built in ${property.year}. Contact us for more details and to schedule a viewing.</p>
           </div>
-          </div>
-          <hr>`;
+        </div>
+      `;
           propertyList.appendChild(propertyItem);
         });
       }
     }
+
+
   })
   .catch(error => console.error('Error fetching data:', error));
